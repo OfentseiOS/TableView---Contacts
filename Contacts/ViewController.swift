db9f1de31ad5882bb1623112ee202964d4ec5d49
Ofentse Malebye <malebyeoc@gmail.com>
2024-03-14 00:21:18 +0200

    Commit

--- Contacts/ViewController.swift
+++ Contacts/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+    
+    let names = [
+        "Mary Jones",
+        "Josephine Smith",
+        "Jane Smith",
+        "Gregory Jones",
+        "Luke Smith",
+        "Gilbert Pi",
+        "Sarah Melborne",
+        "Sharon Murphy",
+        "Peter Murphy",
+        "Abby"
+       
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped me!")
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = names[indexPath.row]
+        
+        return cell
+    }
+}
